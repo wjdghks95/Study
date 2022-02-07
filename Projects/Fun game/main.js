fa8ec176fedfdb1b6abd27f339f874d5c4a8764f
wjdghks95 <wjdghks2799@naver.com>
2022-02-07 17:14:05 +0900

    Create item when startgame

---
+++ Projects/Fun game/main.js
@@ -0,0 +1,53 @@
+CARROUT_COUNT = 5;
+BUG_COUNT = 3;
+
+CARROT_SIZE = 80;
+
+const gameBtn = document.querySelector('.game__button');
+const gameField = document.querySelector('.game__field');
+
+let started = false;
+
+gameBtn.addEventListener('click', () => {
+    if (started === true) {
+        stopGame();
+    };
+    if (started === false) {
+        startGame();
+    };
+});
+
+function startGame() {
+    started = true;
+    initGame();
+};
+
+function initGame() {
+    addItem(CARROUT_COUNT, 'carrot', 'img/carrot.png');
+    addItem(BUG_COUNT, 'bug', 'img/bug.png');
+};
+
+function addItem(count, className, imgPath) {
+    const gameFieldRect = gameField.getBoundingClientRect();
+    const x1 = 0;
+    const x2 = gameFieldRect.width -CARROT_SIZE;
+    const y1 = 0;
+    const y2 = gameFieldRect.height -CARROT_SIZE;
+
+    for (let i = 1; i <= count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        
+        const x = getRandomNumber(x1, x2);
+        const y = getRandomNumber(y1, y2);
+
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        gameField.appendChild(item);
+    };
+};
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+};

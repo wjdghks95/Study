fc20fbf48c6d494e59f6537dfefa5f838a493ed0
wjdghks95 <wjdghks2799@naver.com>
2022-02-13 16:37:42 +0900

    JS: Slide movie chart(Swiper)

--- Projects/Megabox site/src/swiper.use.js
+++ Projects/Megabox site/src/swiper.use.js
@@ -11,3 +11,20 @@ const swiper = new Swiper('.swiper-container', {
    delay: 3000,
  },
 });
+// 배너 이미지 슬라이드
+
+const swipe2 = new Swiper('.swiper-container2', {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    mousewheel: {
+        invert: true,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    autoplay: {
+        delay: 5000,
+    },
+});
+// 영화차트 이미지 슬라이드

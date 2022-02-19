dc346845f6d9f2dc80b1b66bb1a0a7ab8cee8bc2
wjdghks95 <wjdghks2799@naver.com>
2022-02-19 19:53:27 +0900

    JS: Home - Play next video when button is clicked

---
+++ Projects/Hyundai_site/src/main.js
@@ -0,0 +1,67 @@
+(function() {
+	function Slideshow(element) {
+		this.el = document.querySelector( element );
+		this.init();
+	}
+
+	Slideshow.prototype = {
+		init: function() {
+			this.wrapper = this.el.querySelector( ".slider-wrapper" );
+			this.slides = this.el.querySelectorAll( ".slide" );
+			this.previous = this.el.querySelector( ".slider-previous" );
+			this.next = this.el.querySelector( ".slider-next" );
+			this.index = 0;
+			this.total = this.slides.length;
+			
+			this.actions();	
+		},
+		_slideTo: function( slide ) {
+            const currentSlide = this.slides[slide];
+            const video = currentSlide.querySelector('video');
+            currentSlide.style.opacity = 1;
+            video.load();
+			
+			for( var i = 0; i < this.slides.length; i++ ) {
+				var slide = this.slides[i];
+				if( slide !== currentSlide ) {
+					slide.style.opacity = 0;
+				}
+			}
+		},
+		actions: function() {
+			let self = this;
+
+			self.next.addEventListener( "click", function() {
+				self.index++;
+				self.previous.style.display = "block";
+				
+				if( self.index == self.total - 1 ) {
+					self.index = self.total - 1;
+					self.next.style.display = "none";
+				}
+				
+				self._slideTo( self.index );
+				
+			}, false);
+			
+			self.previous.addEventListener( "click", function() {
+				self.index--;
+				self.next.style.display = "block";
+				
+				if( self.index == 0 ) {
+					self.index = 0;
+					self.previous.style.display = "none";
+				}
+				
+				self._slideTo( self.index );
+				
+			}, false);
+		}
+		
+		
+	};
+	
+    const slider = new Slideshow("#main-slider");
+
+})();
+

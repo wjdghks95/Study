0eda6a2de3685abf4b4f54c19969e49bd3c18e30
wjdghks95 <wjdghks2799@naver.com>
2022-03-11 16:31:01 +0900

    Java: Control

---
+++ Language/Java/java-flow-control/BooleanApp.java
@@ -0,0 +1,20 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+//		String true = "Hello World"; reserved word
+		
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("egoing"));
+		
+	}
+
+}

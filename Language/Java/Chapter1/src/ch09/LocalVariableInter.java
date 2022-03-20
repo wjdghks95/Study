e2278d7bc5a0d40c8c09d51ab0441a845bfec93e
wjdghks95 <wjdghks2799@naver.com>
2022-03-20 21:21:30 +0900

    Java: DataType

---
+++ Language/Java/Chapter1/src/ch09/LocalVariableInter.java
@@ -0,0 +1,23 @@
+package ch09;
+
+public class LocalVariableInter {
+
+	public static void main(String[] args) {
+
+		var i = 10;
+		var j = 10.0;
+		var str = "Test";
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(str);
+		
+		var str2 = str;
+		System.out.println(str2);
+		
+		str = "Hello";
+//		str = 3;
+		System.out.println(str);
+	}
+
+}
---
+++ Language/Java/Chapter1/src/ch10/TypeConversionTest.java
@@ -0,0 +1,34 @@
+package ch10;
+
+public class TypeConversionTest {
+
+	public static void main(String[] args) {
+
+    /*	byte bNum = 125;
+		int iNum = bNum;
+		
+		System.out.println(iNum);
+		
+		int iNum2 = 255;
+		byte bNum2 = (byte) iNum2;
+		
+		System.out.println(bNum2);
+		
+		double dNum = 3.14;
+		int iNum3 = (int) dNum;
+		
+		System.out.println(iNum3);
+		
+		*/
+		
+		double dNum = 1.2;
+		float fNum = 0.9F;
+		
+		int iNum1 = (int)dNum + (int)fNum;
+		int iNum2 = (int)(dNum + fNum);
+		
+		System.out.println(iNum1);
+		System.out.println(iNum2);
+	}
+
+}

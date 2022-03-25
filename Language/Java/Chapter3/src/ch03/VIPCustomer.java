dc68e4fc62088a2519ff90587f8ca0e438b462b9
wjdghks95 <wjdghks2799@naver.com>
2022-03-25 21:37:44 +0900

    Java: Inheritance

---
+++ Language/Java/Chapter3/src/ch03/VIPCustomer.java
@@ -0,0 +1,34 @@
+package ch03;
+
+public class VIPCustomer extends Customer {
+	
+	double salesRatio;
+	private String agentID;
+	
+	/* public VIPCustomer() {
+		
+		super(0, null);
+		
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+		
+		System.out.println("VIPCustomer() call");
+
+	}
+	*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		bonusRatio = 0.05;
+		customerGrade = "VIP";
+		salesRatio = 0.1;
+		
+		System.out.println("VIPCustomer(int, String) call");
+	}
+	
+	public String getAgentID() {
+		return agentID;
+	}
+}
---
+++ Language/Java/Chapter3/src/ch06/VIPCustomer.java
@@ -0,0 +1,39 @@
+package ch06;
+
+public class VIPCustomer extends Customer {
+	
+	double salesRatio;
+	private String agentID;
+	
+	/* public VIPCustomer() {
+		
+		super(0, null);
+		
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+		
+		System.out.println("VIPCustomer() call");
+
+	}
+	*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		bonusRatio = 0.05;
+		customerGrade = "VIP";
+		salesRatio = 0.1;
+	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		return price;
+	}
+
+	public String getAgentID() {
+		return agentID;
+	}
+}

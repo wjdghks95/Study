381e7992c45a3e73edf7ce47cee6b8df2d07b31d
wjdghks95 <wjdghks2799@naver.com>
2022-03-31 21:23:10 +0900

    Java: String Class, Class Class

---
+++ Language/Java/Chapter4/src/ch03/StringTest.java
@@ -0,0 +1,18 @@
+package ch03;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+
+		String java = new String("java");
+		String android = new String("android");
+		
+		System.out.println(System.identityHashCode(java));
+		System.out.println(java);
+		java = java.concat(android);
+		
+		System.out.println(System.identityHashCode(java));
+		System.out.println(java);
+	}
+
+}

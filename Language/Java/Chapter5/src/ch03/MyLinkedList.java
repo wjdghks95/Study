c8e4ad11c8555292edaa3bff8a5462a98a48ad02
wjdghks95 <wjdghks2799@naver.com>
2022-04-05 19:11:14 +0900

    Java: Data Structure-LinkedList

--- Language/Java/Chapter5/src/ch03/MyLinkedList.java
+++ Language/Java/Chapter5/src/ch03/MyLinkedList.java
@@ -11,78 +11,74 @@ public class MyLinkedList {
 		count = 0;
 	}
 	
-	public MyListNode addElement( String data )
-	{
-		
+	public MyListNode addElement( String data ) {
 		MyListNode newNode;
-		if(head == null){  //맨 처음일때
+		
+		if(head == null) {
 			newNode = new MyListNode(data);
 			head = newNode;
-		}
-		else{
+		} else {
 			newNode = new MyListNode(data);
 			MyListNode temp = head;
-			while(temp.next != null)  //맨 뒤로 가서  
+			while(temp.next != null) {
 				temp = temp.next;
+			}
 			temp.next = newNode;
 		}
+		
 		count++;
+		
 		return newNode;
 	}
 	
-	public MyListNode insertElement(int position, String data )
-	{
+	public MyListNode insertElement(int position, String data ) {
+		
 		int i;
 		MyListNode tempNode = head;
+		MyListNode preNode = null;
+		
 		MyListNode newNode = new MyListNode(data);
 		
-		if(position < 0 || position > count ){
-			System.out.println("추가 할 위치 오류 입니다. 현재 리스트의 개수는 " + count +"개 입니다.");
+		if(position < 0 || position > count) {
 			return null;
 		}
 		
-		if(position == 0){  //맨 앞으로 들어가는 경우
+		if(position == 0) {
 			newNode.next = head;
 			head = newNode;
-		}
-		else{
-			MyListNode preNode = null;	
-			for(i=0; i<position; i++){
+		} else {
+			for(i=0; i<position; i++) {
 				preNode = tempNode;
 				tempNode = tempNode.next;
-				
 			}
+			
 			newNode.next = preNode.next;
 			preNode.next = newNode;
 		}
+		
 		count++;
-		return newNode;
+		return newNode;		
+		
 	}
 	
-	public MyListNode removeElement(int position)
-	{
+	public MyListNode removeElement(int position) {
+		
 		int i;
 		MyListNode tempNode = head;
+		MyListNode preNode = null;
 		
-		if(position >= count ){
-			System.out.println("삭제 할 위치 오류입니다. 현재 리스트의 개수는 " + count +"개 입니다.");
-			return null;
-		}
-		
-		if(position == 0){  //맨 앞을 삭제하는
+		if(position == 0) {
 			head = tempNode.next;
-		}
-		else{
-			MyListNode preNode = null;	
-			for(i=0; i<position; i++){
+		} else {
+			for(i=0; i<position; i++) {
 				preNode = tempNode;
 				tempNode = tempNode.next;
 			}
+			
 			preNode.next = tempNode.next;
 		}
-		count--;
-		System.out.println(position + "번째 항목 삭제되었습니다.");
 		
+		count--;
 		return tempNode;
 	}
 	

6222afb0be49a209a2a4c5fbf6983cb93fdc5104
wjdghks95 <wjdghks2799@naver.com>
2022-04-14 18:17:39 +0900

    Java: I,O Stream

---
+++ Language/Java/Chapter6/src/ch15/FileReaderTest2.java
@@ -0,0 +1,27 @@
+package ch15;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileReaderTest2 {
+
+	public static void main(String[] args) {
+
+		try(FileWriter fw = new FileWriter("writer.txt")) {
+			
+			fw.write("A");
+			char buf[] = {'B', 'C', 'D', 'E', 'F', 'G'};
+			
+			fw.write(buf);
+			fw.write("안녕하세요");
+			fw.write(buf, 1, 2);
+			fw.write("65");
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("출력이 완료되었습니다.");
+	}
+
+}

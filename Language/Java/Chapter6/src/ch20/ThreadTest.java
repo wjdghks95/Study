106e440ae5be09c31fab3833ab0ddd9c65661c16
wjdghks95 <wjdghks2799@naver.com>
2022-04-15 18:23:08 +0900

    Java: Thread

---
+++ Language/Java/Chapter6/src/ch20/ThreadTest.java
@@ -0,0 +1,47 @@
+package ch20;
+
+//class MyThread extends Thread {
+class MyThread implements Runnable {
+
+	@Override
+	public void run() {
+		
+		int i;
+		for(i=1; i<=200; i++) {
+			System.out.print(i + "\t");
+		}		
+	}
+
+}
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Thread.currentThread() + "start");
+	
+		MyThread runnable = new MyThread();
+		
+		Thread th1 = new Thread(runnable);
+		Thread th2 = new Thread(runnable);
+		
+/*		MyThread th1 = new MyThread();
+		MyThread th2 = new MyThread();
+*/		
+		th1.start();
+		th2.start();
+
+		System.out.println(Thread.currentThread() + "end");
+		
+		Runnable run = new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				System.out.println("run");
+			}
+		};
+		
+		run.run();
+	}
+}

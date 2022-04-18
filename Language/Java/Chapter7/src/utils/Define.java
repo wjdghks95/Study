df034206cfdb86d08e22deabef4881d2fd36aaf3
wjdghks95 <wjdghks2799@naver.com>
2022-04-18 21:17:57 +0900

    Java: mini project

---
+++ Language/Java/Chapter7/src/utils/Define.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Define {
+
+	public static final int KOREA = 1001; // 국어
+	public static final int MATH = 2001;  // 수학
+	public static final int DANCE = 3001; // 방송댄스
+	
+	public static final int AB_TYPE = 0;  // A, B, C
+	public static final int SAB_TYPE = 1; // S, A, B, C
+	public static final int PF_TYPE = 2;  // P, F
+}

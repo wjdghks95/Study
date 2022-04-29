689d4d20ca9ad36c7a882e1f4c296ada7732c8fb
wjdghks95 <wjdghks2799@naver.com>
2022-04-29 16:52:39 +0900

    Facade pattern

--- Framwork/design/src/main/java/Main.java
+++ Framwork/design/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import design.adapter.*;
 import design.aop.AopBrowser;
 import design.decorator.*;
+import design.facade.Ftp;
+import design.facade.Reader;
+import design.facade.SftpClient;
+import design.facade.Writer;
 import design.observer.Button;
 import design.observer.IButtonListener;
 import design.proxy.Browser;
@@ -79,7 +83,7 @@ public class Main {
         ICar audi5 = new A5(audi, "A5");
         audi5.showPrice();*/
 
-        Button button = new Button("버튼");
+       /* Button button = new Button("버튼");
         button.addListener(new IButtonListener() {
             @Override
             public void clickEvent(String event) {
@@ -89,7 +93,33 @@ public class Main {
         button.click("메시지 전달 : click 1");
         button.click("메시지 전달 : click 2");
         button.click("메시지 전달 : click 3");
-        button.click("메시지 전달 : click 4");
+        button.click("메시지 전달 : click 4");*/
+
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisConnect();
+        ftpClient.disConnect();
+
+        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+        sftpClient.connect();
+
+        sftpClient.write();
+
+        sftpClient.read();
+
+        sftpClient.disConnect();
+
     }
 
     // 콘센트

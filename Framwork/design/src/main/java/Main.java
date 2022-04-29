b93e58a7a08748a18e269f0cfd9280dc5ca6f8fe
wjdghks95 <wjdghks2799@naver.com>
2022-04-29 16:02:56 +0900

    Proxy Pattern

--- Framwork/design/src/main/java/Main.java
+++ Framwork/design/src/main/java/Main.java
@@ -1,7 +1,12 @@
 import design.adapter.*;
+import design.aop.AopBrowser;
+import design.proxy.Browser;
+import design.proxy.IBrowser;
 import design.singleton.Aclazz;
 import design.singleton.BClazz;
 import design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +20,7 @@ public class Main {
         System.out.println("두 개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));*/
 
-        HairDryer hairDryer = new HairDryer();
+        /*HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
         Cleaner cleaner = new Cleaner();
@@ -24,11 +29,42 @@ public class Main {
 
         AirConditioner arAirConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(arAirConditioner);
-        connect(airAdapter);
+        connect(airAdapter);*/
+
+        /*Browser browser = new Browser("www.naver.com");
+        browser.show();*/
+
+        /*IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();*/
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
     }
 
     // 콘센트
     public static void connect(Electronic110V electronic110V) {
+
         electronic110V.powerOn();
     }
 }
---
+++ Framwork/design/src/main/java/design/proxy/Html.java
@@ -0,0 +1,10 @@
+package design.proxy;
+
+public class Html {
+
+    private String url;
+
+    public Html(String url) {
+        this.url = url;
+    }
+}

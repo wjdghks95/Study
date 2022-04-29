0651fcfa561ca23e209378ffd1fb868ef9244576
wjdghks95 <wjdghks2799@naver.com>
2022-04-29 15:34:42 +0900

    Adapter pattern

--- Framwork/design/src/main/java/Main.java
+++ Framwork/design/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import design.adapter.*;
 import design.singleton.Aclazz;
 import design.singleton.BClazz;
 import design.singleton.SocketClient;
@@ -5,13 +6,29 @@ import design.singleton.SocketClient;
 public class Main {
     public static void main(String[] args) {
 
-        Aclazz aClazz = new Aclazz();
+        /*Aclazz aClazz = new Aclazz();
         BClazz bClazz = new BClazz();
 
         SocketClient aClient = aClazz.getSocketClient();
         SocketClient bClient = bClazz.getSocketClient();
 
         System.out.println("두 개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
+        System.out.println(aClient.equals(bClient));*/
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner arAirConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(arAirConditioner);
+        connect(airAdapter);
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }

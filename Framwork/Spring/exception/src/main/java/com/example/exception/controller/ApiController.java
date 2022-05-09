40a18b579d2a2d2fc7baeed72b13e99f2ff2e46c
wjdghks95 <wjdghks2799@naver.com>
2022-05-09 16:57:03 +0900

    Spring: Validation, Exception

--- Framwork/Spring/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ Framwork/Spring/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -3,22 +3,31 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
-
-        int a = 10 + age;
 
         return user;
     }
@@ -29,9 +38,9 @@ public class ApiController {
         return user;
     }
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("api controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
-    }
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        System.out.println("api controller");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
+//    }
 }

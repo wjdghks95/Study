931fa71386dd088fc5ae352cd8be184bdd41e5fc
wjdghks95 <wjdghks2799@naver.com>
2022-05-13 12:49:21 +0900

    Spring mini project: Front View

---
+++ Framwork/Spring/restaurant/src/main/java/com/example/restaurant/config/MvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.restaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60 * 60 * 24 * 365);
+    }
+}

f2eca625ab9facee51b473ac08b69104372b9176
wjdghks95 <wjdghks2799@naver.com>
2022-05-19 21:57:56 +0900

    Spring: 의존관계 자동 주입

--- Framwork/inflearn_Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Framwork/inflearn_Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,22 +1,59 @@
 package hello.core.order;
 
+import hello.core.annoatation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    /*
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    private MemberRepository memberRepository;
+    @Autowired
+    private DiscountPolicy discountPolicy;
+    */
+
+    /*
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    */
+
+    /*@Autowired
+    private DiscountPolicy rateDiscountPolicy;*/
+
+    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+
+    /*
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

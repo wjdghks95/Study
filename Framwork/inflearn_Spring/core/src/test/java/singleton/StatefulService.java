3b73910004a5306cfbef0ef99127f1d350fccad0
wjdghks95 <wjdghks2799@naver.com>
2022-05-19 16:03:24 +0900

    Spring: 싱글톤 컨테이너

---
+++ Framwork/inflearn_Spring/core/src/test/java/singleton/StatefulService.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드
+
+    /*public void order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        this.price = price; // 문제 부분
+    }*/
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        return price;
+    }
+
+/*    public int getPrice() {
+        return price;
+    }*/
+}

922766d45e0145f3f14dcb413fe6b9b8b359caff
wjdghks95 <wjdghks2799@naver.com>
2022-06-16 20:16:52 +0900

    Spring: 데이터 접근 기술 - Jpa

--- Framwork/inflearn_Spring/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ Framwork/inflearn_Spring/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,15 +5,10 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -68,7 +63,7 @@ class ItemRepositoryTest {
         Long itemId = savedItem.getId();
 
         //when
-        ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
+        ItemUpdateDto updateParam = new ItemUpdateDto("Item", 20000, 30);
         itemRepository.update(itemId, updateParam);
 
         //then
@@ -82,20 +77,20 @@ class ItemRepositoryTest {
     void findItems() {
         //given
         Item item1 = new Item("itemA-1", 10000, 10);
-        Item item2 = new Item("itemA-2", 20000, 20);
+        Item item = new Item("itemA-2", 20000, 20);
         Item item3 = new Item("itemB-1", 30000, 30);
 
         itemRepository.save(item1);
-        itemRepository.save(item2);
+        itemRepository.save(item);
         itemRepository.save(item3);
 
         //둘 다 없음 검증
-        test(null, null, item1, item2, item3);
-        test("", null, item1, item2, item3);
+        test(null, null, item1, item, item3);
+        test("", null, item1, item, item3);
 
         //itemName 검증
-        test("itemA", null, item1, item2);
-        test("temA", null, item1, item2);
+        test("itemA", null, item1, item);
+        test("temA", null, item1, item);
         test("itemB", null, item3);
 
         //maxPrice 검증

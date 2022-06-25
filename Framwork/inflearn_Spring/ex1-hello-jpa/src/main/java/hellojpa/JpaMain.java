01e520dd354da07ececdcf95d58f45cabf296463
wjdghks95 <wjdghks2799@naver.com>
2022-06-25 13:12:53 +0900

    Spring: 영속성 관리

--- Framwork/inflearn_Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Framwork/inflearn_Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAA");
 
+            em.detach(member);
+//            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
+
+            System.out.println("======================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();

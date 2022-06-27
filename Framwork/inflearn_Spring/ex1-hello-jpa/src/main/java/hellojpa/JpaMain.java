0e44387cbe9fc8d989e302b0478af2c6b02c56b8
wjdghks95 <wjdghks2799@naver.com>
2022-06-27 15:09:29 +0900

    Spring: 고급 매핑

--- Framwork/inflearn_Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Framwork/inflearn_Spring/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -18,15 +18,14 @@ public class JpaMain {
         try {
 
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("user");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
             em.persist(member);
 
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-
-            em.persist(team);
+            em.flush();
+            em.clear();
 
             tx.commit();
         } catch (Exception e) {

3e4dad5e812aa7f41e691b57a72feb5ab5828c35
wjdghks95 <wjdghks2799@naver.com>
2022-06-30 21:03:23 +0900

    Spring: 스프링 JPA 활용 - 컬렉션 조회 최적화

--- Framwork/inflearn_Spring/jpashop2/src/main/java/jpabook/jpashop2/domain/OrderItem.java
+++ Framwork/inflearn_Spring/jpashop2/src/main/java/jpabook/jpashop2/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop2.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop2.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;

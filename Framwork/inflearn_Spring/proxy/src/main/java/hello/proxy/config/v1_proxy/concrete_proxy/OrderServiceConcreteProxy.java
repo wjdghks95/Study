3804aee5ea04114286bdaa2da047982cf8c040f1
wjdghks95 <wjdghks2799@naver.com>
2022-07-07 13:00:16 +0900

    Spring: Proxy 패턴과 Decorator 패턴

---
+++ Framwork/inflearn_Spring/proxy/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,33 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+    private final LogTrace trace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace trace) {
+        super(null);
+        this.target = target;
+        this.trace = trace;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.request()");
+            // target 호출
+            target.orderItem(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}

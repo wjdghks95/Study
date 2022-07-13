8dd35734542042a951062904d558f9b966e2f282
LJH9308 <92257691+LJH9308@users.noreply.github.com>
2022-07-13 09:47:26 +0900

    복습 - 빈 생명주기 콜백

---
+++ Framwork/Spring/practice/src/test/java/hello/springcore/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package hello.springcore.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Bean 생명주기 콜백
+ */
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    /**
+     * initMethod, @PostConstruct: 초기화 콜백
+     * destroyMethod, @PreDestroy: 소멸전 콜백
+     */
+    @Configuration
+    static class LifeCycleConfig {
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}

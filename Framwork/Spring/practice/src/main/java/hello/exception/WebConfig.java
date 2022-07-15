c1106643f2717acdbe67fb56ade9a7a4d389e65a
LJH9308 <92257691+LJH9308@users.noreply.github.com>
2022-07-15 10:29:31 +0900

    복습 - 예외처리와 오류페이지

---
+++ Framwork/Spring/practice/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,42 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+/**
+ * 예외 페이지 호출 시 Filter 또는 Interceptor 가 중복 호출되는 문제 해결
+ *  - Filter: DispatchType 으로 중복 호출 제거 (dispatchType=REQUEST), Default: REQUEST
+ *  - Interceptor: 경로 정보로 중복 호출 제거(excludePathPatterns("/error-page/**"))
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/*")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico"
+                        , "/error", "/error-page/**" //오류 페이지 경로
+                );
+    }
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
---
+++ Framwork/Spring/practice/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,27 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * 예외 페이지 요청 Customizer
+ * WebServerFactoryCustomizer<ConfigurableWebServerFactory> 인터페이스 구현
+ *
+ * 스프링 부트는 /error 를 기본으로 요청
+ * BasicErrorController 요청을 처리하여 templates/error/* 의 View 를 반환
+ */
+//@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}

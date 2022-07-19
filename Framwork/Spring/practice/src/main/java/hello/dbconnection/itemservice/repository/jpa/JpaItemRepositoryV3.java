5c665abfe51eee76b916ed95970637fe38e25b23
LJH9308 <92257691+LJH9308@users.noreply.github.com>
2022-07-19 21:32:52 +0900

    복습 - 데이터 접근 기술: Querydsl

---
+++ Framwork/Spring/practice/src/main/java/hello/dbconnection/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -0,0 +1,109 @@
+package hello.dbconnection.itemservice.repository.jpa;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.dbconnection.itemservice.domain.Item;
+import hello.dbconnection.itemservice.domain.QItem;
+import hello.dbconnection.itemservice.repository.ItemRepository;
+import hello.dbconnection.itemservice.repository.ItemSearchCond;
+import hello.dbconnection.itemservice.repository.ItemUpdateDto;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static hello.dbconnection.itemservice.domain.QItem.item;
+
+/**
+ * Querydsl
+ * Querydsl 사용 시 JPAQueryFactory 필요
+ * JPAQueryFactory 는 JPQL 을 만들기 때문에 EntityManager 필요
+ *
+ * where
+ *  - BooleanBuilder
+ *  - BooleanExpression
+ */
+@Repository
+@Transactional
+public class JpaItemRepositoryV3 implements ItemRepository {
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public JpaItemRepositoryV3(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    public List<Item> findAllOld(ItemSearchCond itemSearch) {
+        String itemName = itemSearch.getItemName();
+        Integer maxPrice = itemSearch.getMaxPrice();
+        QItem item = QItem.item;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (StringUtils.hasText(itemName)) {
+            builder.and(item.itemName.like("%" + itemName + "%"));
+        }
+        if (maxPrice != null) {
+            builder.and(item.price.loe(maxPrice));
+        }
+
+        List<Item> result = query
+                .select(item)
+                .from(item)
+                .where(builder)
+                .fetch();
+
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        List<Item> result = query
+                .select(item)
+                .from(item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+        return result;
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+}

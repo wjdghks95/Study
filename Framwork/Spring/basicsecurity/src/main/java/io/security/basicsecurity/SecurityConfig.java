9c2e29b2eac4ef2aa41294e74ec67d08e4e0ea10
wjdghks95 <wjdghks2799@naver.com>
2022-08-05 22:29:57 +0900

    Spring Security - Logout과 LogoutFilter

--- Framwork/Spring/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Framwork/Spring/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -52,6 +55,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 }) // 로그인 실패 후 핸들러
                 .permitAll()
+        ;
+
+        http
+                .logout() // 로그아웃 처리, LogoutFilter 동작
+                .logoutUrl("/logout") // 로그아웃 처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                }) // 로그아웃 핸들러
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                }) // 로그아웃 성공 후 핸들러
+                .deleteCookies("remember-me") // 로그아웃 후 쿠키 삭제
         ;
     }
 }

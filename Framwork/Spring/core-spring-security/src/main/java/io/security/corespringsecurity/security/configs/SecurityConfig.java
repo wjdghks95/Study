04cf5992c0a21508fff6e700393384e28f5e187f
wjdghks95 <wjdghks2799@naver.com>
2022-08-08 13:46:54 +0900

    Spring Security - WebIgnore 설정과 사용자 DB 등록

--- Framwork/Spring/core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ Framwork/Spring/core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -35,12 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); // StaticResource 보안 적용X
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         /** 인가 */
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/users").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")

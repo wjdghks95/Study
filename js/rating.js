034ee00f906d51a6f9afec064ca4534fd3f0451b
DESKTOP-EI3B66C\user1 <wjdghks2799@github.com>
2022-09-21 18:27:41 +0900

    Upload-review: rating - js

---
+++ js/rating.js
@@ -0,0 +1,40 @@
+let stars = document.querySelectorAll('.rating__star');
+let ratDisplay = document.querySelector('.rating_display');
+let totalStar = 0;
+
+stars.forEach((star, index) => {
+    star.dataset.rating = index + 1;
+    star.addEventListener('mouseover', onMouseOver);
+    star.addEventListener('click', onClick);
+    star.addEventListener('mouseleave', onMouseLeave);
+});
+
+function onMouseOver(e) {
+    const ratingVal = e.target.dataset.rating;
+    if (!ratingVal) {
+        return;
+    } else {
+        fill(ratingVal);
+    }
+}
+
+function fill(ratingVal) {
+    for (let i = 0; i < 5; i++) {
+        if (i < ratingVal) {
+            stars[i].querySelector('img').setAttribute('src', '../icon/star-regular.svg');
+        } else {
+            stars[i].querySelector('img').setAttribute('src', '../icon/star-soild.svg');
+        }
+    }
+}
+
+function onMouseLeave(e) {
+    fill(totalStar);
+}
+
+function onClick(e) {
+    const ratingVal = e.target.dataset.rating;
+    totalStar = ratingVal;
+    fill(totalStar);
+    ratDisplay.innerHTML = ratingVal;
+}

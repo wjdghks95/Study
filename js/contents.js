780f37ece4543e0432fc086f4d74461a50df5e11
{jeonghwan} <wjdghks2799@github.com>
2022-10-04 17:32:43 +0900

    viewer 버튼 활성화

---
+++ js/contents.js
@@ -0,0 +1,31 @@
+const viewerBtns = document.querySelectorAll('.category__viewer-button');
+const contents = document.querySelectorAll('.category__content');
+
+viewerBtns.forEach(viewerBtn => {
+    viewerBtn.addEventListener('click', () => {
+        viewerBtns.forEach(viewerBtn => {
+            viewerBtn.classList.remove('active');
+        })
+        viewerBtn.classList.add('active');
+        
+        if(viewerBtns[0].classList.contains('active')) {
+            contents[0].style.display = 'flex';
+        } else {
+            contents[0].style.display = 'none';
+        }
+        
+        if(viewerBtns[1].classList.contains('active')) {
+            contents[1].style.display = 'table';
+        } else {
+            contents[1].style.display = 'none';
+        }
+
+        if(viewerBtns[2].classList.contains('active')) {
+            contents[2].style.display = 'flex';
+        } else {
+            contents[2].style.display = 'none';
+        }
+        
+    })
+})
+

60aa9ee0f4acb1ed74992e622d0367e3483324ca
{jeonghwan} <wjdghks2799@github.com>
2022-10-06 18:15:09 +0900

    build gradle

---
+++ rol/src/main/java/wjdghks95/project/rol/RolApplication.java
@@ -0,0 +1,13 @@
+package wjdghks95.project.rol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolApplication.class, args);
+	}
+
+}
---
+++ rol/src/test/java/wjdghks95/project/rol/RolApplicationTests.java
@@ -0,0 +1,13 @@
+package wjdghks95.project.rol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

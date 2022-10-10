190da38afcfbeef6863195dc9318350f7d2b2f1d
wjdghks95 <wjdghks2799@Naver.com>
2022-10-10 21:13:53 +0900

    EnableJpaAuditing

--- rol/src/main/java/wjdghks95/project/rol/RolApplication.java
+++ rol/src/main/java/wjdghks95/project/rol/RolApplication.java
@@ -3,12 +3,13 @@ package wjdghks95.project.rol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableJpaAuditing
 public class RolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RolApplication.class, args);
 	}
-
 }

d2f1962df224f4155b537023d7a01df02a083388
JEONGHWANLEE <wjdghks2799@Naver.com>
2024-02-13 20:52:25 +0900

    토비의 스프링 - 프로젝트 세팅

---
+++ Framwork/Spring/springbook/src/main/java/com/example/springbook/SpringbookApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbookApplication.class, args);
+	}
+
+}
+                   

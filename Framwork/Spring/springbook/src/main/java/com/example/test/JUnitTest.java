d197eef926904868575ced5ecaa613d9ea786009
JEONGHWANLEE <wjdghks2799@Naver.com>
2024-02-19 20:14:41 +0900

    토비의 스프링 - 2.2 UserDaoTest 개선

---
+++ Framwork/Spring/springbook/src/main/java/com/example/test/JUnitTest.java
@@ -0,0 +1,9 @@
+package com.example.test;
+
+import org.junit.runner.JUnitCore;
+
+public class JUnitTest {
+    public static void main(String[] args) {
+        JUnitCore.main("com.example.test.UserDaoTest");
+    }
+}

1f84c23782e26962cad20dee5c2019e0494f1de0
JEONGHWANLEE <wjdghks2799@Naver.com>
2024-03-23 10:09:44 +0900

    토비의 스프링 - 7.1 SQL과 DAO의 분리

---
+++ Framwork/Spring/springbook/src/main/java/com/example/springapplication/SqlService.java
@@ -0,0 +1,15 @@
+package com.example.springapplication;
+
+public interface SqlService {
+    String getSql(String key);
+
+    public class SqlRetrievalFailureException extends RuntimeException {
+        public SqlRetrievalFailureException(String message) {
+            super(message);
+        }
+
+        public SqlRetrievalFailureException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
---
+++ Framwork/Spring/springbook/src/main/java/com/example/springapplication/UserDaoJdbc.java
@@ -0,0 +1,85 @@
+package com.example.springapplication;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class UserDaoJdbc implements UserDao {
+//    private String sqlAdd;
+
+//    public void setSqlAdd(String sqlAdd) {
+//        this.sqlAdd = sqlAdd;
+//    }
+
+//    private Map<String, String> sqlMap;
+
+//    public void setSqlMap(Map<String, String> sqlMap) {
+//        this.sqlMap = sqlMap;
+//    }
+
+    private SqlService sqlService;
+
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            user.setLevel(Level.valueOf(rs.getInt("level")));
+            user.setLogin(rs.getInt("login"));
+            user.setRecommend(rs.getInt("recommend"));
+            user.setEmail(rs.getString("email"));
+
+            return user;
+        }
+    };
+
+    @Override
+    public void add(final User user)  {
+//        this.jdbcTemplate.update(this.sqlAdd, user.getId(), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+//        this.jdbcTemplate.update(this.sqlMap.get("add"), user.getId(), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+        this.jdbcTemplate.update(this.sqlService.getSql("userAdd"), user.getId(), user.getName(), user.getPassword(), user.getEmail(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+    }
+
+    @Override
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), new Object[]{id}, this.userMapper);
+    }
+
+    @Override
+    public void deleteAll() {
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
+    }
+
+    @Override
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGetCount"), Integer.class);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"), this.userMapper);
+    }
+
+    @Override
+    public void update(User user) {
+        this.jdbcTemplate.update(this.sqlService.getSql("userUpdate"),
+                user.getName(), user.getPassword(), user.level.intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
+    }
+}

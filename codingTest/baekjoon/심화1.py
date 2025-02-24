9a39d94ac5715fc2d088af473888188296afd424
JEONGHWANLEE <wjdghks2799@Naver.com>
2025-02-24 23:04:11 +0900

    코딩테스트 - 백준_심화1

--- codingTest/baekjoon/심화1.py
+++ codingTest/baekjoon/심화1.py
@@ -116,3 +116,74 @@ for i in arr:
         break
 
 print(result)
+
+
+
+# 크로아티아 알파벳
+""" 
+문제
+예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다.
+크로아티아 알파벳	변경
+        č	        c=
+        ć	        c-
+        dž	        dz=
+        đ	        d-
+        lj	        lj
+        nj	        nj
+        š	        s=
+        ž	        z=
+예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
+
+입력
+첫째 줄에 최대 100글자의 단어가 주어진다. 알파벳 소문자와 '-', '='로만 이루어져 있다.
+단어는 크로아티아 알파벳으로 이루어져 있다. 문제 설명의 표에 나와있는 알파벳은 변경된 형태로 입력된다.
+
+출력
+입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+"""
+
+text = input()
+alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+"""
+count = 0
+for a in alphabet:
+    count += text.count(a)
+    text = text.replace(a, " ")
+
+count += len(text.replace(" ", "").strip())
+"""
+for i in alphabet:
+    text = text.replace(i,'*')
+
+print(len(text))
+
+
+
+# 그룹 단어 체커
+""" 
+문제
+그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 
+예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 
+둘째 줄부터 N개의 줄에 단어가 들어온다. 
+단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+
+출력
+첫째 줄에 그룹 단어의 개수를 출력한다.
+"""
+N = int(input())
+for _ in range(N):
+    word = input()
+    
+    for c in word:
+        subWord = word[word.index(c):word.rindex(c)+1]
+        subWord = subWord.replace(c, '')
+
+        if (len(subWord) > 0):
+            N -= 1
+            break
+print(N)

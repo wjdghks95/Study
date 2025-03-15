9bcb36aaa0a0a09477ecf4f6030c43e713d4e0f2
JEONGHWANLEE <wjdghks2799@Naver.com>
2025-03-15 10:59:51 +0900

    알고리즘 - 이진탐색

---
+++ algorithm/2st_week/02_09_is_existing_target_number_binary_2.py
@@ -0,0 +1,9 @@
+finding_target = 2
+finding_numbers = [0, 3, 5, 6, 1, 2, 4]
+
+def is_exist_target_number_binary(target, array):
+    return True
+
+
+result = is_exist_target_number_binary(finding_target, finding_numbers)
+print(result)
